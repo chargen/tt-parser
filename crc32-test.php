7661c4662f3957e14070f7615048acb60c9c0edc
Bert Huijben <bert@vmoo.com>
2013-02-06 01:41:07 +0100

    Tweak test

--- crc32-test.php
+++ crc32-test.php
@@ -1,20 +1,19 @@
 <?php
 
-$val = 'banaan';
+$val = 'banaan12';
 $c = crc32($val);
 $h = hash("crc32b", $val);
 
 
 print('CRC32: ' . $c . "\n");
 print('hex: ' . sprintf('0x%08x', $c) . "\n");
+print('DecHex: ' .dechex($c) . "\n");
+print('Dec: ' . $c . "\n");
 print("\n");
 
-print('Via Hask: ' . $h . "\n");
+print('Via Hash: ' . $h . "\n");
 $i = intval($h, 16);
 print('hex: ' . sprintf('0x%08x', $i) . "\n");
+print('DecHex: ' .dechex($i) . "\n");
 print('Dec: ' . $i . "\n");
-
-// Both hex lines should have the same value
-// The final 'Dec:' line should have the same value as the top 'CRC32'
-
 
